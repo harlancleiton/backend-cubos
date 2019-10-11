9d3b70bb99b58fd48c81abda62cd1e7da296d6d7
Harlan Cleiton <harlancleiton@gmail.com>
2019-10-11 00:01:14 -0300

    RegrasValidator.store

---
+++ src/controllers/validators/regras.validator.ts
@@ -0,0 +1,37 @@
+import { NextFunction, Request, Response } from 'express';
+import * as moment from 'moment';
+import { Regra } from '../../dto';
+import { regraModel } from '../../models/regra.model';
+
+class RegrasValidator {
+  store(req: Request, res: Response, next: NextFunction) {
+    const dto: Regra = req.body;
+    const regras: Regra[] = regraModel.findAll()!;
+    const inicio = moment(dto.inicio, 'HH:mm');
+    const fim = moment(dto.fim, 'HH:mm');
+
+    let valido: boolean = true;
+
+    for (const regra of regras) {
+      const regraInicio = moment(regra.inicio, 'HH:mm');
+      const regraFim = moment(regra.fim, 'HH:mm');
+      if (
+        regraInicio.isBetween(inicio, fim, 'milliseconds', '[]') ||
+        regraFim.isBetween(inicio, fim, 'milliseconds', '[]')
+      ) {
+        valido = false;
+        break;
+      }
+    }
+
+    if (valido) {
+      next();
+    } else {
+      res.status(400).send({
+        message: 'O horário informado conflita com uma regra existente',
+      });
+    }
+  }
+}
+
+export default new RegrasValidator();
--- src/routes.ts
+++ src/routes.ts
@@ -1,10 +1,11 @@
 import { Router } from 'express';
 import horariosController from './controllers/horarios.controller';
 import regrasController from './controllers/regras.controller';
+import regrasValidator from './controllers/validators/regras.validator';
 
 const routes = Router();
 
-routes.post('/regras', regrasController.store);
+routes.post('/regras', [regrasValidator.store, regrasController.store]);
 routes.get('/regras', regrasController.index);
 routes.get('/regras/:id', regrasController.show);
 routes.delete('/regras/:id', regrasController.destroy);
